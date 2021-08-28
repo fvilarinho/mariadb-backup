e542e361a59c47ae0a296a1ac85119db78a45001
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2021-08-28 19:29:07 -0300

    Code improvements.

--- stop.sh
+++ stop.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+BUILD_NAME=`jq -r .build.name.value ./etc/settings.json`
 BUILD_VERSION=`jq -r .build.version.value ./etc/settings.json`
 
-echo "BUILD_VERSION=$BUILD_VERSION" > .env
+echo "BUILD_NAME=$BUILD_NAME" > .env
+echo "BUILD_VERSION=$BUILD_VERSION" >> .env
 
 docker-compose down
 
